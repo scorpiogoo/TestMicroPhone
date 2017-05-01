1c989a56ba6baf62d58a3b07dee43921fec5b686
Dave Jensen <screamingchameleons1@gmail.com>
2017-05-01 14:06:24 -0600

    Created an IntentHandlerFactory, an IntentHandler interface, and a first implementation for the PlayTrackIntent.

---
+++ app/src/main/java/com/example/ben/testmicrophone/IntentHandler.java
@@ -0,0 +1,8 @@
+package com.example.ben.testmicrophone;
+
+/**
+ * Created by dave on 5/1/17.
+ */
+
+public interface IntentHandler {
+}
---
+++ app/src/main/java/com/example/ben/testmicrophone/PlayTrackHandler.java
@@ -0,0 +1,10 @@
+package com.example.ben.testmicrophone;
+
+import IntentHandler;
+
+/**
+ * Created by dave on 5/1/17.
+ */
+
+public class PlayTrackHandler implements IntentHandler {
+}
